d9f4f84f24fb389cfe3cf34ad57248444a066aa8
insimitsu <37982344+insimitsu@users.noreply.github.com>
2018-07-10 11:56:39 +0200

    Updated code

--- Fibbonacci.java
+++ Fibbonacci.java
@@ -1,20 +1,21 @@
 
-package Fibbonacci;
-import java.util.*;
-class Fibbonacci{
-public static void main(String args[]) {
-    int f1=0, f2=1, f3, i, limit;
-    Scanner insLim = new Scanner(System.in);
-    System.out.println("Ciag Fibbonacciego");
-    System.out.println("Wprowadz limit liczb z ciagu (od 1 do 25:");
-    limit = insLim.nextInt();
-    System.out.print(f1 + " " + f2 + " ");
-        for (i = 2; i<=limit; i++){
-            f3=f1+f2;
-            System.out.print(f3 + " ");
-            f1=f2;
-            f2=f3;
+import java.util.Scanner;
+
+public class Fibonacci {
+        public static void main(String args[]) {
+            int f1=0, f2=1, f3, i, howManyNumbers;
+            Scanner userInput = new Scanner(System.in);
+            System.out.println("Fibonacci Numbers");
+            System.out.println();
+            System.out.print("Type in how many numbers you'd like to see(limit:30): ");
+            howManyNumbers = userInput.nextInt();
+            System.out.print(f1 + " " + f2 + " ");
+            for (i = 2; i<=howManyNumbers-1; i++){
+                f3=f1+f2;
+                System.out.print(f3 + " ");
+                f1=f2;
+                f2=f3;
             }
-        System.out.println();
-        }     
+            System.out.println();
+        }
 }
